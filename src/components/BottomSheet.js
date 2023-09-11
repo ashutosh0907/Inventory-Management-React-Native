893cbecc1ba8ef9f8682e11e0a916184015074c4
asimgope <asimgope53@gmail.com>
2023-09-11 11:24:37 +0530

    modal bottomsheet

--- src/components/BottomSheet.js
+++ src/components/BottomSheet.js
@@ -1,52 +1,25 @@
 import React, { useState } from 'react';
-import { BottomSheet, Button, ListItem } from '@rneui/themed';
-import { Alert, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Modal from 'react-native-modal';
 
-const BottomSheetComponent = ({ isVisible, toggleBottomSheet }) => {
-  const list = [
-    { title: 'Open Camera' },
-    { title: 'Open gallery' },
-  ];
-
-  const handleListItemPress = (item) => {
-    console.log('Selected Item:', item.title);
-    Alert.alert(
-        'Selected Item',
-        item.title,
-        
-    )
-
-    toggleBottomSheet(false); // Close the bottom sheet
-  };
-
+const BottomSheetModal = ({ isVisible, onClose }) => {
   return (
-    <>
-      <Button
-        title="Open Bottom Sheet"
-        onPress={() => toggleBottomSheet(true)}
-        buttonStyle={styles.button}
-      />
-      <BottomSheet modalProps={{}} isVisible={isVisible}>
-        {list.map((l, i) => (
-          <ListItem
-            key={i}
-            containerStyle={l.containerStyle}
-            onPress={() => handleListItemPress(l)}
-          >
-            <ListItem.Content>
-              <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
-            </ListItem.Content>
-          </ListItem>
-        ))}
-      </BottomSheet>
-    </>
+    <Modal
+      isVisible={isVisible}
+      onBackdropPress={onClose}
+      animationIn="slideInUp" // Add the slide-up animation
+      animationOut="slideOutDown" // Add the slide-down animation
+    >
+      <View style={{ flex: 1, justifyContent: 'flex-end', margin: 0 }}>
+        <View style={{ backgroundColor: 'white', padding: 16 }}>
+          <Text>This is your BottomSheet content.</Text>
+          <TouchableOpacity onPress={onClose}>
+            <Text>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
-const styles = StyleSheet.create({
-  button: {
-    margin: 10,
-  },
-});
-
-export default BottomSheetComponent;
+export default BottomSheetModal;
--- src/screens/Truckunloading.js
+++ src/screens/Truckunloading.js
@@ -1,21 +1,41 @@
-import { View, Text } from 'react-native';
 import React, { useState } from 'react';
-import BottomSheetComponent from '../components/BottomSheet';
+import { View, Text, Modal, TouchableOpacity } from 'react-native';
 
-const Truckunloading = () => {
-    const [isVisible, setIsVisible] = useState(false);
-    return (
-        <View>
-            <Text>Truckunloading</Text>
-            <BottomSheetComponent
-                isVisible={isVisible}
-                toggleBottomSheet={() => {
-                    console.log('toggle bottom sheet');
-                    setIsVisible(!isVisible); // Toggle visibility
-                }}
-            />
+const TruckUnloading = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const onClose = () => {
+    setIsVisible(false);
+  };
+
+  return (
+    <View style={{ flex: 1, backgroundColor: 'green' }}>
+      <Text
+        onPress={() => {
+          setIsVisible(true);
+        }}
+        style={{ padding: 20, backgroundColor: 'blue', color: 'white', textAlign: 'center' }}
+      >
+        Truck Unloading
+      </Text>
+      <Modal
+        isVisible={isVisible}
+        onBackdropPress={onClose}
+        animationIn="slideInUp"
+        animationOut="slideOutDown"
+        style={{ margin: 0 }}
+      >
+        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+          <View style={{ backgroundColor: 'white', padding: 16 }}>
+            <Text>This is your BottomSheet content.</Text>
+            <TouchableOpacity onPress={onClose}>
+              <Text>Close</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-    );
-}
+      </Modal>
+    </View>
+  );
+};
 
-export default Truckunloading;
+export default TruckUnloading;
