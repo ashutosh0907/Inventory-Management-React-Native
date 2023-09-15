1553333d00bdad861c518ce663470b80b3db44e7
ashutosh <ashutoshmohapatra0907@gmail.com>
2023-09-15 21:08:08 +0530

    Made splash screen with login box

---
+++ src/components/TextInputName.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { HEIGHT, STYLES, WIDTH } from "../constants/config";
+import { BLACK, Border_Color, GRAY, WHITE } from "../constants/color";
+import { RFValue } from "react-native-responsive-fontsize";
+import LinearGradient from "react-native-linear-gradient";
+
+export const TextInputName = ({
+  title = "",
+  value = "",
+  placeholder = "",
+  width = '100%',
+  // keyboardType="default",
+  // maxLength=0,
+  onChangeText,
+  MarginVertical = 0,
+  // editable="default"
+}) => {
+
+  return (
+    <>
+      <View style={{
+        width: width,
+      }}>
+        <View style={{
+        }}>
+          <Text style={{
+            color: BLACK,
+            fontWeight: 'bold',
+            fontSize: 16,
+            paddingLeft: 5
+          }}>
+            {title}
+          </Text>
+        </View>
+        <View style={{
+          borderRadius: 7,
+          borderWidth: 0.6,
+          borderColor: WHITE,
+          marginTop: 5,
+        }}>
+          <LinearGradient
+            end={{ x: 1, y: 1 }}
+            start={{ x: 0, y: 2 }}
+            colors={['rgba(255, 255, 255, 0.3)', 'rgba(255, 255, 255, 0.3)', 'rgba(100, 100, 100, 0.4)',]}
+            style={{
+              borderRadius: 7
+            }}>
+            <TextInput
+              editable={true}
+              onChangeText={(txt) => {
+                if (!/[0-9,.*-@]/.test(txt.slice(-1))) {
+                  onChangeText(txt);
+                } else {
+                }
+              }}
+              value={value}
+              // keyboardType="default"
+              placeholder={placeholder}
+              placeholderTextColor={'#444654'}
+              style={{
+                ...Styles.inputTextStyle,
+              }}
+            // keyboardType={keyboardType}
+            // maxLength={maxLength}
+            />
+          </LinearGradient>
+        </View>
+      </View>
+
+
+
+    </>
+  )
+}
+const Styles = StyleSheet.create({
+  ViewForTextInput: {
+    //   ...STYLES.elevation,
+    height: 50,
+    //backgroundColor:WHITE,
+    alignSelf: 'center',
+    borderRadius: 7,
+    borderWidth: 0.6,
+    borderColor: WHITE,
+    marginTop: 5
+
+  },
+  inputTextStyle: {
+    fontSize: 15,
+    color: BLACK,
+    //borderRadius:10,
+    // fontFamily: "Roboto-Regular",
+    fontWeight: 'bold',
+    marginLeft: 5,
+  },
+});
--- src/loginpages/Login.js
+++ src/loginpages/Login.js
@@ -1,37 +1,138 @@
-import { View, Text, StyleSheet, ToastAndroid } from 'react-native'
-import React from 'react'
-import { BLACK } from '../constants/color'
+import { View, Text, StyleSheet, ScrollView, ToastAndroid, ImageBackground, Modal, Pressable, TouchableOpacity, Image, KeyboardAvoidingView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { BLACK, WHITE } from '../constants/color'
 import { storeObjByKey } from '../utils/Storage'
 import { checkuserToken } from '../redux/actions/auth'
 import { useDispatch } from "react-redux";
+import { GALLERY, LOGO, TRUCK } from '../constants/imagepath'
+import { HEIGHT, MyStatusBar, WIDTH } from '../constants/config'
+import { TextInputName } from '../components/TextInputName'
 
 const Login = ({ navigation }) => {
   const dispatch = useDispatch();
+  const [showlogin, setLogin] = useState('false');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLogin(true)
+    }, 2000)
+  }, [])
   return (
-    <View style={{
-      ...styles.mainContainer
-    }}>
-      <Text
-        style={{
-          color: BLACK
-        }}
-        onPress={() => {
-          storeObjByKey('loginResponse', { name: 'Ashutosh', id: 1 }).then(() => {
-            dispatch(checkuserToken());
-          })
-          ToastAndroid.show('Login Successful', ToastAndroid.SHORT)
-        }}
-      >Login</Text>
-    </View>
+    <React.Fragment>
+      <MyStatusBar backgroundColor='#7ca8d5' barStyle={'dark-content'} />
+      <KeyboardAvoidingView behavior='height' style={{
+        flex: 1
+      }}>
+        <ImageBackground resizeMode='cover' source={TRUCK} style={styles.image}>
+          <Modal
+            visible={showlogin}
+            transparent={true}
+            animationType='fa'
+            statusBarTranslucent
+            onRequestClose={() => { }}>
+            <MyStatusBar backgroundColor='#7ca8d5' barStyle={'dark-content'} />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+              <View
+                style={{
+                  width: WIDTH,
+                  height: HEIGHT,
+                  backgroundColor: `rgba(100, 100, 100, 0.0)`,
+                  alignSelf: 'center',
+                }}>
+                <View style={{
+                  width: '100%',
+                  height: '22%',
+                  alignItems: 'center',
+                  alignSelf: 'center',
+                  justifyContent: 'flex-end',
+                  paddingBottom: 20
+                }}>
+                  <Image
+                    style={{
+                      width: '25%',
+                      height: '62%',
+                      borderRadius: 100
+                    }}
+                    resizeMode={'center'}
+                    source={LOGO}
+                  />
+                </View>
+                <View style={{
+                  backgroundColor: 'rgba(255, 255, 255, 0.25)',
+                  width: '85%',
+                  height: '30%',
+                  paddingTop: 20,
+                  paddingBottom: 12,
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 7,
+                  borderWidth: 1,
+                  borderColor: WHITE,
+                }}>
+                  <TextInputName
+                    value={username}
+                    title='Username'
+                    placeholder='username'
+                    width='90%'
+                    onChangeText={setUsername}
+                  />
+                  <TextInputName
+                    value={password}
+                    title='Password'
+                    placeholder='password'
+                    width='90%'
+                    onChangeText={setPassword}
+                  />
+                  <TouchableOpacity onPress={() => {
+                    // storeObjByKey('loginResponse', { name: 'Ashutosh', id: 1 }).then(() => {
+                    //   dispatch(checkuserToken());
+                    // })
+                    console.log(username, " : ", password);
+                  }} style={{
+                    backgroundColor: 'rgba(100, 100, 100, 0.3)',
+                    width: WIDTH * 0.35,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignSelf: 'center',
+                    borderRadius: 4,
+                    marginVertical: 10,
+                    borderWidth: 1,
+                    borderColor: WHITE,
+                    marginTop: 20
+                  }}>
+                    <Text style={{ color: BLACK, fontSize: 17, padding: 10, fontWeight: 'bold' }}>Login</Text>
+                  </TouchableOpacity>
+                </View>
+              </View >
+            </ScrollView>
+          </Modal>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    </React.Fragment>
   )
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+  },
+  image: {
+    // flex: 1,
+    width: '100%',
+    height: HEIGHT,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0
+    // width: '100%',
+    // height: '100%'
+    // justifyContent: 'center',
+  },
 })
 
 export default Login
