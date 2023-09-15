02a7642ad4b47a0db9bdc9be7541d52c2fefd4fb
asimgope <asimgope53@gmail.com>
2023-09-15 21:41:13 +0530

    loaderupdtae

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,69 +1,41 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View ,Modal,Image} from 'react-native'
-import { STYLES, WIDTH } from "../constants/config";
-import { BLACK, Border_Color, WHITE } from "../constants/color";
-import { LOADER } from "../constants/imagepath";
+import React from "react";
+import { Image, Modal, Pressable, ScrollView, View } from "react-native";
+import { HEIGHT, MyStatusBar } from "../constants/config";
+import { EMI_LOADER, LOADER } from "../constants/imagepath";
 
 export const Loader = ({
-    // title="",
-    // value = "",
-    // placeholder = "",
-    // keyboardType="default",
-    // maxLength=0,
-    // onChangeText,
-    // MarginVertical=0,
-    // editable="default"
-}) =>{
+    visible=false,
+    onBackPress,
+    imageStyle,
+    source,
+    backgroundColor=`rgba(100, 100, 100, 0.5)`,
+}) => {
+    return (
+        <Modal
+            visible={visible}
+            transparent={true}
+            // animationType='slide'
+            statusBarTranslucent
+            onRequestClose={() =>onBackPress!=undefined && onBackPress(false)}
+        >
+            <Pressable 
+            
+            style={{
+                flex: 1,
+                justifyContent:'center',
+                backgroundColor: backgroundColor,
+                alignItems: 'center'
+            }}>
+            <MyStatusBar barStyle={'dark-content'} />
 
-
-    return(
-        <>
-  <Modal
-    visible={loading}
-    onRequestClose={() => {
-      setLoading(false);
-    }}
-    transparent
-    statusBarTranslucent
-    hardwareAccelerated
-  >
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "rgba(40, 40, 40, 0.3)",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Image
-        style={{
-          height: '100%',
-          width: '100%',
-        }}
-        source={LOADER}
-      />
-    </View>
-  </Modal>
-        </>
+                <Image
+                    style={{
+                        height:100,
+                        width:100
+                    }}
+                    source={LOADER}
+                />
+            </Pressable>
+        </Modal>
     )
 }
-const Styles = StyleSheet.create({
-    ViewForTextInput: {
-    //   ...STYLES.elevation,
-      height:50,
-      width:WIDTH * 0.9,
-      //backgroundColor:WHITE,
-      alignSelf:'center',
-      borderRadius:7,
-      borderWidth:0.6,
-      borderColor:WHITE,
-      marginTop:5
-
-    },
-    inputTextStyle: {
-      fontSize:12,
-      color:WHITE,
-      //borderRadius:10,
-      marginLeft: 5,
-    },
-  });
